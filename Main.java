c361b7fe6c69d8a5ab586223c0cc5f153f4789d7
shashank <gouthamshashank@gmail.com>
2021-09-03 16:13:25 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.gou;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		AdminDepartment admin = new AdminDepartment();
+		HRDepartment hr = new HRDepartment();
+		TechDepartment tech = new TechDepartment();
+		
+		System.out.println("Welcome to"+admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday());
+		
+		System.out.println("\nWelcome to"+hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		
+		System.out.println("\n\nWelcome to"+tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+		
+	}
+
+}
